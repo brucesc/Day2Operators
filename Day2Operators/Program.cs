b6425136d11f11a7828a43f6a83e8a379597b5c4
Steven Bruce <Brucesc726@gmail.com>
2018-02-23 13:48:41 -0500

    initial

--- Day2Operators/Program.cs
+++ Day2Operators/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Day2Operators
+class Solution
 {
-    class Program
+
+    static void Main(String[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        double meal_cost = Convert.ToDouble(Console.ReadLine());
+        int tip_percent = Convert.ToInt32(Console.ReadLine());
+        int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+        double adjTipPercent = Convert.ToDouble(tip_percent / 100.00);
+        double adjTaxPercent = Convert.ToDouble(tax_percent / 100.00);
+
+
+        double tip = meal_cost * adjTipPercent;
+        double tax = meal_cost * adjTaxPercent;
+
+        double totalCost = meal_cost + tip + tax;
+
+        double roundedTotal = Math.Round(totalCost);
+
+        Console.Write($"The total meal cost is {roundedTotal} dollars.");
+        Console.ReadKey();
+
+
+
+
+
     }
 }
